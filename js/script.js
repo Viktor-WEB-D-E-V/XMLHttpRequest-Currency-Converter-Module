e754216684d71a043ab070376db48ec2946eb6ec
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-03-06 20:10:02 -1000

    Fixed typo

--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@ inputUah.addEventListener("input", () => {
   const request = new XMLHttpRequest();
 
   request.open("GET", "js/current.json");
-  request.setRequestHeader("Content-type", "app;ication/json; charset=utf-8");
+  request.setRequestHeader("Content-type", "appication/json; charset=utf-8");
   request.send();
 
   request.addEventListener("readystatechange", () => {
-    if (request.status === 200) {
+    if (request.readyState === 4 && request.status === 200) {
       const data = JSON.parse(request.response);
       inputUsd.value = +(inputUah.value / data.current.usd).toFixed(2);
     } else {
-      inputUsd.value = "Something going wrong";
+      inputUsd.value = "SSomething went wrong";
     }
   });
 });
